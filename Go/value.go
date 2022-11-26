da84e1d70b1a641b3d5c7ebd4a15dd8a209a8225
Yuan Li <liyuan.montreal@gmail.com>
2022-11-25 23:34:53 -0500

    add example for go

---
+++ Go/value.go
@@ -0,0 +1,30 @@
+package main
+import "fmt"
+func main() {
+Strings, which can be added together with +.
+
+    fmt.Println("go" + "lang")
+Integers and floats.
+
+    fmt.Println("1+1 =", 1+1)
+    fmt.Println("7.0/3.0 =", 7.0/3.0)
+Booleans, with boolean operators as you’d expect.
+
+    fmt.Println(true && false)
+    fmt.Println(true || false)
+    fmt.Println(!true)
+}
+
+/*Go has various value types including strings, integers, floats, booleans, 
+etc. Here are a few basic examples.*/
+
+
+/*
+$ go run values.go
+golang
+1+1 = 2
+7.0/3.0 = 2.3333333333333335
+false
+true
+false
+*/
